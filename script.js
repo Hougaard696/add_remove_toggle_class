1357892be7be90ee74277fde7aa4fdbb157efcaf
Christiaan Hougaard <cwhougaard@gmail.com>
2021-05-29 16:39:46 -0400

    final save

---
+++ script.js
@@ -0,0 +1,6 @@
+$(function() {
+  // Write the jquery to  fill in the span when you hover one of the span.
+  // Hint: See the CSS provided for class names
+
+  // Write the jQuery to clear all drawings when the user clicks the clear button
+});
