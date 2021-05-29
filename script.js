4e69153185071985319d3411b9068c536d2cc15c
Christiaan Hougaard <cwhougaard@gmail.com>
2021-05-29 16:48:02 -0400

    creating on hover function

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(function() {
   // Write the jquery to  fill in the span when you hover one of the span.
+  $(".grid").on('onhover', function () {
+    
+  })
   // Hint: See the CSS provided for class names
 
   // Write the jQuery to clear all drawings when the user clicks the clear button
